1fe5d6820bfdc441b0592bd4e47cdcc92ea10dc1
sonu <sonumagar787@gmail.com>
2025-07-26 11:53:11 +0545

    updated

--- src/main/java/com/furEverHome/controller/PetCenterController.java
+++ src/main/java/com/furEverHome/controller/PetCenterController.java
@@ -1,7 +1,6 @@
 package com.furEverHome.controller;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.furEverHome.dto.PetRequest;
--- src/main/java/com/furEverHome/repository/PetRepository.java
+++ src/main/java/com/furEverHome/repository/PetRepository.java
@@ -1,13 +1,19 @@
 package com.furEverHome.repository;
 
-import com.furEverHome.entity.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.furEverHome.entity.Pet;
+
 public interface PetRepository extends JpaRepository<Pet, UUID> {
-	List<Pet> findByCenterId(UUID centerId);
-	List<Pet> searchAvailableByNameAndLocation(@Param("name") String name, @Param("location") String location);
+    List<Pet> findByCenterId(UUID centerId);
+    Optional<Pet> findById(UUID id);
+
+    @Query("SELECT p FROM Pet p WHERE p.centerId = :centerId AND LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')) AND LOWER(p.location) LIKE LOWER(CONCAT('%', :location, '%'))")
+    List<Pet> searchByCenterIdAndNameAndLocation(@Param("centerId") UUID centerId, @Param("name") String name, @Param("location") String location);
 }
